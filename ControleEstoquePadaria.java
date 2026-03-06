0e8c888c8260ea9d622f5a60dc11b5943a419371
Sara <saragiovana738@gmail.com>
2026-03-06 14:35:53 -0300

    Add files via upload

---
+++ ControleEstoquePadaria.java
@@ -0,0 +1,7 @@
+public class ControleEstoquePadaria {  
+	public static void main (String[] args) {
+		int quantidadeFarinha = 10;
+		int quantidadeAcucar = 5;
+		System.out.println("A receita requer " + quantidadeFarinha + " xícaras de farinha e " + quantidadeAcucar + " xícaras de açúcar.");
+	}
+}
