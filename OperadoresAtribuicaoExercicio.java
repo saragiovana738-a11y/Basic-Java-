be40cc8f699a83974149ac585b0ae30ab2a3d5d8
Sara <saragiovana738@gmail.com>
2026-03-06 14:36:37 -0300

    Add files via upload

---
+++ OperadoresAtribuicaoExercicio.java
@@ -0,0 +1,22 @@
+public class OperadoresAtribuicaoExercicio {
+	public static void main(String[] args) {
+		int numero = 5;
+		
+		int adicao = numero;
+		adicao += 3;
+		int subtracao = numero;
+		subtracao -= 2;
+		int multiplicacao = numero;
+		multiplicacao *= 4;
+		double divisao = numero;
+		divisao /= 2;
+		
+
+		System.out.println(adicao);
+		System.out.println(subtracao);
+		System.out.println(multiplicacao);
+		System.out.println(divisao);
+
+	}
+}
+
