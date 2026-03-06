f61f90d3e669eace5c8f9f072f8764fd4572e59b
saragiovana738-a11y <saragiovana738@gmail.com>
2026-03-06 14:22:50 -0300

    Add files via upload
    Java program that calculates area of a rectangle

---
+++ CalculadoraAreaRetangulo.java
@@ -0,0 +1,10 @@
+public class CalculadoraAreaRetangulo {
+	public static void main (String [] args) {
+		double largura = 5.0;
+		double altura = 3.0;
+		double area = largura * altura;
+
+		System.out.println("A area do triângulo é " + area);
+	}
+}
+
