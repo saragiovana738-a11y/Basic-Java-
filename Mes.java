9e69dfe6cfb76ef8b3c5c8a81eb77a7561c1d1da
Sara <saragiovana738@gmail.com>
2026-03-06 14:36:49 -0300

    Add files via upload

---
+++ Mes.java
@@ -0,0 +1,23 @@
+public class Mes {
+	public static void main(String[] args) {
+		
+		int mes = 6;
+
+		switch (mes) {
+
+			case 1 -> System.out.println("Janeiro");
+
+			case 2 -> System.out.println("Fevereiro");
+
+			case 3 -> System.out.println("Março");
+
+			case 4 -> System.out.println("Abril");
+
+			case 5 -> System.out.println("Maio");
+
+			default -> System.out.println("Desconhecido");
+		}
+		
+	}
+}
+
