6fffcdd3b4be89297ef91bf7f7a65476351b1aa1
Sara <saragiovana738@gmail.com>
2026-04-13 13:20:12 -0300

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,11 @@
+import exercicio.dados.Produto;
+public class Info {
+	public static void main(String[] args) {
+	
+	
+	Produto p1 = new Produto("Garrafa", 12.99, 100);
+
+	p1.exibirProduto();
+
+	}
+}
