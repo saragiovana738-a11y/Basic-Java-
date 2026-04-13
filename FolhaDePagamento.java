7bb377122ffafe28dcd0c85ead9c9892043ebf9c
Sara <saragiovana738@gmail.com>
2026-04-13 13:20:44 -0300

    Add files via upload

---
+++ FolhaDePagamento.java
@@ -0,0 +1,7 @@
+import empresa.dados.*;
+public class FolhaDePagamento {
+	public static void main(String[] args) {
+		Funcionario funcionario = new Funcionario("Arnaldo", 5000.00);
+		System.out.println(funcionario.obterInfo());
+	}
+}
